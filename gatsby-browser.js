b5f3aac7efd4b2db030dcc0dd3224a793422b013
elegant-moon <elegantmoon.tech@gmail.com>
2020-04-22 04:29:23 +0200

    Goal 3: Setting up Menu and hamburger for mobile

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,9 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 
 import "prismjs/themes/prism.css"
+
+// global styles ( burger menu, )
+import "./src/styles/global.css"
+
+// reactstrap styles
+import "bootstrap/dist/css/bootstrap.css"
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,69 +1,95 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import { rhythm, scale } from "../utils/typography"
+
+import { slide as Menu } from "react-burger-menu"
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+  Container,
+} from "reactstrap"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   let header
 
-  if (location.pathname === rootPath) {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
+
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+  if (isMobile) {
     header = (
-      <h1
-        style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
+      <div id="header">
+        <Menu>
+          <a id="cases" className="menu-item" href="/cases">
+            Cases
+          </a>
+          <a id="post-a-case" className="menu-item" href="/post-a-case">
+            Post a Case
+          </a>
+          <a id="subscribe" className="menu-item" href="/subscribe">
+            Subscribe
+          </a>
+        </Menu>
+      </div>
     )
   } else {
     header = (
-      <h3
-        style={{
-          fontFamily: `Montserrat, sans-serif`,
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h3>
+      <div id="header">
+        <Container>
+          <Navbar color="dark" dark expand="md">
+            <NavbarBrand href="/">Logo</NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                <NavItem>
+                  <NavLink href="/cases/">Cases</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/post-a-case/">Post a Case</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/subscribe/">Subscribe</NavLink>
+                </NavItem>
+              </Nav>
+              <NavbarText>Social Icons</NavbarText>
+            </Collapse>
+          </Navbar>
+        </Container>
+      </div>
     )
   }
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
+    <>
       <header>{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
-    </div>
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    </>
   )
 }
 
