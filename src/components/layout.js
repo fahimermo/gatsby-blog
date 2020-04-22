157918a3f6dfb4d2d8879d50f3d624591ee708fc
elegant-moon <elegantmoon.tech@gmail.com>
2020-04-22 17:02:35 +0200

    Deploy on Netlify

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 import { rhythm, scale } from "../utils/typography"
@@ -23,54 +23,58 @@ import {
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
-  let header
+  //let header
 
   const [isOpen, setIsOpen] = useState(false)
   const toggle = () => setIsOpen(!isOpen)
 
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
-  if (isMobile) {
-    header = (
-      <div id="header">
-        <Menu>
-          <a id="cases" className="menu-item" href="/cases">
-            Cases
-          </a>
-          <a id="post-a-case" className="menu-item" href="/post-a-case">
-            Post a Case
-          </a>
-          <a id="subscribe" className="menu-item" href="/subscribe">
-            Subscribe
-          </a>
-        </Menu>
-      </div>
-    )
-  } else {
-    header = (
-      <div id="header">
-        <Container>
-          <Navbar color="dark" dark expand="md">
-            <NavbarBrand href="/">Logo</NavbarBrand>
-            <NavbarToggler onClick={toggle} />
-            <Collapse isOpen={isOpen} navbar>
-              <Nav className="mr-auto" navbar>
-                <NavItem>
-                  <NavLink href="/cases/">Cases</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/post-a-case/">Post a Case</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/subscribe/">Subscribe</NavLink>
-                </NavItem>
-              </Nav>
-              <NavbarText>Social Icons</NavbarText>
-            </Collapse>
-          </Navbar>
-        </Container>
-      </div>
-    )
-  }
+  const [header, setHeader] = useState("")
+  useEffect(() => {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+    if (isMobile) {
+      setHeader(
+        <div id="header">
+          <Menu>
+            <a id="cases" className="menu-item" href="/cases">
+              Cases
+            </a>
+            <a id="post-a-case" className="menu-item" href="/post-a-case">
+              Post a Case
+            </a>
+            <a id="subscribe" className="menu-item" href="/subscribe">
+              Subscribe
+            </a>
+          </Menu>
+        </div>
+      )
+    } else {
+      setHeader(
+        <div id="header">
+          <Container>
+            <Navbar color="dark" dark expand="md">
+              <NavbarBrand href="/">Logo</NavbarBrand>
+              <NavbarToggler onClick={toggle} />
+              <Collapse isOpen={isOpen} navbar>
+                <Nav className="mr-auto" navbar>
+                  <NavItem>
+                    <NavLink href="/cases/">Cases</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/post-a-case/">Post a Case</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/subscribe/">Subscribe</NavLink>
+                  </NavItem>
+                </Nav>
+                <NavbarText>Social Icons</NavbarText>
+              </Collapse>
+            </Navbar>
+          </Container>
+        </div>
+      )
+    }
+  }, [])
+
   return (
     <>
       <header>{header}</header>
